2bf373eed5f17404cf8b5192761f3e27d129157c
Willem Wollebrants <willem@studiow.be>
2017-01-13 20:52:24 +0100

    Add filter constants

--- src/FiltersInterface.php
+++ src/FiltersInterface.php
@@ -6,6 +6,14 @@ namespace Lune\Osmosis;
 
 interface FiltersInterface
 {
+
+    const FILTER_EQUALS = 'equals';
+    const FILTER_IN_ARRAY = 'inArray';
+    const FILTER_GREATER_THAN = 'greaterThan';
+    const FILTER_GREATER_THAN_OR_EQUAL = 'greaterThanOrEqual';
+    const FILTER_LESS_THAN = 'lessThan';
+    const FILTER_LESS_THAN_OR_EQUAL = 'lessThanOrEqual';
+
     public function equals(string $fieldname, $value, bool $strict = true):FiltersInterface;
 
     public function inArray(string $fieldname, $value, bool $strict = true):FiltersInterface;
